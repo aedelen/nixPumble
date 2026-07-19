4eee45e2e4c8012589cd220758de6a1e62cfc79a
Adam Edelen <aedelen42@gmail.com>
2026-07-19 08:15:52 -0500

    allowing unfree

--- flake.nix
+++ flake.nix
@@ -6,21 +6,25 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in
     {
       # Allows you to run or build the package directly from this folder
       packages.${system} = {
-        pumble = pkgs.callPackage ./pumble.nix {};
+        pumble = pkgs.callPackage ./pumble.nix { };
         default = self.packages.${system}.pumble;
       };
 
       # An overlay makes it simple to inject Pumble directly into your system-wide pkgs
       overlays.default = final: prev: {
-        pumble = final.callPackage ./pumble.nix {};
+        pumble = final.callPackage ./pumble.nix { };
       };
     };
 }
