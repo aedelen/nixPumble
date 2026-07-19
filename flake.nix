f0cc666fcb3175d723750a000efa9ba72d5d826e
Adam Edelen <aedelen42@gmail.com>
2026-07-19 07:26:11 -0500

    first commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A flake for the Pumble desktop messaging application";
+
+  inputs = {
+    # Using unstable as Electron apps usually need the latest dependencies
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      # Allows you to run or build the package directly from this folder
+      packages.${system} = {
+        pumble = pkgs.callPackage ./pumble.nix {};
+        default = self.packages.${system}.pumble;
+      };
+
+      # An overlay makes it simple to inject Pumble directly into your system-wide pkgs
+      overlays.default = final: prev: {
+        pumble = final.callPackage ./pumble.nix {};
+      };
+    };
+}
---
+++ pumble.nix
@@ -0,0 +1,116 @@
+{ lib
+, stdenv
+, fetchurl
+, dpkg
+, autoPatchelfHook
+, makeWrapper
+, alsa-lib
+, atk
+, at-spi2-core
+, cairo
+, cups
+, dbus
+, expat
+, fontconfig
+, freetype
+, gdk-pixbuf
+, glib
+, gtk3
+, nspr
+, nss
+, pango
+, libX11
+, libXcomposite
+, libXdamage
+, libXext
+, libXfixes
+, libXrandr
+, libXrender
+, libXtst
+, libxcb
+, libxshmfence
+, libdrm
+, mesa
+, systemd
+, xdg-utils
+}:
+
+stdenv.mkDerivation rec {
+  pname = "pumble-desktop";
+  version = "1.3.32"; # Check pumble.com/apps for the latest version string
+
+  src = fetchurl {
+    url = "https://pumble.com/download/desktop/linux/Pumble-linux-${version}.deb";
+    # Leave this placeholder string; we will let Nix tell us the real hash next
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  nativeBuildInputs = [
+    dpkg
+    autoPatchelfHook
+    makeWrapper
+  ];
+
+  buildInputs = [
+    alsa-lib
+    atk
+    at-spi2-core
+    cairo
+    cups
+    dbus
+    expat
+    fontconfig
+    freetype
+    gdk-pixbuf
+    glib
+    gtk3
+    nspr
+    nss
+    pango
+    libX11
+    libXcomposite
+    libXdamage
+    libXext
+    libXfixes
+    libXrandr
+    libXrender
+    libXtst
+    libxcb
+    libxshmfence
+    libdrm
+    mesa
+    systemd
+  ];
+
+  unpackPhase = "dpkg-deb -x $src .";
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/bin $out/share
+    cp -r usr/share/* $out/share/
+    cp -r opt $out/opt
+
+    # Make a symlink to the main binary so Nix adds it to your PATH
+    ln -s $out/opt/Pumble/pumble $out/bin/pumble
+
+    # Fix up the shortcut file to point to our newly linked path
+    substituteInPlace $out/share/applications/pumble.desktop \
+      --replace "/opt/Pumble/pumble" "$out/bin/pumble"
+
+    runHook postInstall
+  '';
+
+  postFixup = ''
+    # Tells the app how to launch default browsers when you click links
+    wrapProgram $out/bin/pumble \
+      --prefix PATH : ${lib.makeBinPath [ xdg-utils ]}
+  '';
+
+  meta = with lib; {
+    description = "Pumble team collaboration and messaging app";
+    homepage = "https://pumble.com";
+    license = licenses.unfree;
+    platforms = [ "x86_64-linux" ];
+  };
+}
